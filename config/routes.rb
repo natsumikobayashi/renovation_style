8c70117f55d69a55b1766b0b1be6a41f0a384a38
NatsumiKobayashi <115065917+natsumikobayashi@users.noreply.github.com>
2023-01-18 14:01:34 +0000

    public routes 編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,51 +1,5 @@
 Rails.application.routes.draw do
 
-  namespace :observer do
-    get 'taste_tags/index'
-  end
-  namespace :observer do
-    get 'customers/index'
-  end
-  namespace :observer do
-    get 'admins/index'
-  end
-  namespace :observer do
-    get 'homes/top'
-  end
-  namespace :admin do
-    get 'rooms/index'
-    get 'rooms/new'
-    get 'rooms/confirm'
-    get 'rooms/show'
-  end
-  namespace :admin do
-    get 'enquiries/index'
-    get 'enquiries/show'
-  end
-  namespace :admin do
-    get 'managers/show'
-    get 'managers/confirm'
-  end
-  namespace :admin do
-    get 'homes/top'
-  end
-  namespace :public do
-    get 'rooms/index'
-    get 'rooms/show'
-  end
-  namespace :public do
-    get 'enquiries/index'
-    get 'enquiries/show'
-    get 'enquiries/new'
-    get 'enquiries/confirm'
-  end
-  namespace :public do
-    get 'customers/show'
-    get 'customers/edit'
-    get 'customers/index'
-  end
-  namespace :public do
-  end
   #顧客用
   devise_for :customers, skip:[:passwords], controllers: {
     registrations: "public/registrations",
@@ -54,14 +8,41 @@ Rails.application.routes.draw do
 
   root to: "public/homes#top"
 
+  scope module: :public do
+    patch 'customers/subscribe' => 'customers#subscribe'
+    resource :customers, only: [:show, :edit, :update]
+    get 'enquiries/confirm' => 'enquiries#confirm'
+    resources :enquiries, only: [:index, :show, :new, :create]
+    resources :rooms, only: [:show, :index]
+  end
+
   #リフォーム会社用
   devise_for :managers, skip:[:passwords], controllers: {
     registrations: "admin/registrations",
     sessions: 'admin/sessions'
   }
+    namespace :admin do
+    get 'rooms/index'
+    get 'rooms/new'
+    get 'rooms/confirm'
+    get 'rooms/show'
+    get 'enquiries/index'
+    get 'enquiries/show'
+    get 'managers/show'
+    get 'managers/confirm'
+    get 'homes/top'
+  end
+
   #管理者用
   devise_for :observers, skip:[:passwords, :registrations], controllers: {
     sessions: "observer/sessions"
   }
+
+  namespace :observer do
+    get 'taste_tags/index'
+    get 'customers/index'
+    get 'admins/index'
+    get 'homes/top'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
