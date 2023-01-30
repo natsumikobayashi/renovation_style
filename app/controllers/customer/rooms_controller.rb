7db7ebab811086dfe18f40360a46a968efe69143
NatsumiKobayashi <115065917+natsumikobayashi@users.noreply.github.com>
2023-01-30 13:15:20 +0000

    customer検索機能

--- app/controllers/customer/rooms_controller.rb
+++ app/controllers/customer/rooms_controller.rb
@@ -1,9 +1,19 @@
 class Customer::RoomsController < ApplicationController
   def index
     @rooms = Room.all.page(params[:page]).per(10)
+
      if params[:search]
-       @rooms = @rooms.where(taste_tag_id: params.dig(:search, :taste_tag_ids)) if params.dig(:search, taste_tag_ids)
-       @rooms = @rooms.where(floor_plan_id: params.dig(:search, :floor_plan_ids)) if params.dig(:search, floor_plan_ids)
+       @taste_tag_ids = params[:search][:taste_tag_ids]
+       @area_ids =  params[:search][:area_ids]
+       @floor_plan_ids = params[:search][:floor_plan_ids]
+       @rooms = Room.includes(:manager).where(managers: {area_id: @area_ids == [""] ? Area.pluck(:id) : @area_ids  })
+       .where(taste_tag_id: @taste_tag_ids == [""] ? TasteTag.pluck(:id) : @taste_tag_ids )
+       .where(floor_plan_id: @floor_plan_ids == [""] ? FloorPlan.pluck(:id) : @floor_plan_ids )
+       .page(params[:page]).per(10)
+
+       #@rooms = @rooms.where(taste_tag_id: params.dig(:search, :taste_tag_ids)) if params.dig(:search, taste_tag_ids)
+       #@rooms = @rooms.where(floor_plan_id: params.dig(:search, :floor_plan_ids)) if params.dig(:search, floor_plan_ids)
+       #@rooms = @rooms.where(area_id: params.dig(:search, :area_ids)) if params.dig(:search, area_ids)
      end
   end
 
